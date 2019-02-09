74638ffdc37adeb782bdf1ad7c1971d539c381b1
Konstantin <wetarmor@gmail.com>
2019-02-09 20:02:06 +0300

    Added CVE model

--- NVD/backend/models.py
+++ NVD/backend/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class CVE(models.Model):
+    cve_id = models.CharField(max_length=15, null=False)
+    cvss_score = models.FloatField()
+    access_vector = models.CharField(max_length=50)
+    access_complexity = models.CharField(max_length=15)
+    authentication = models.CharField(max_length=15)
+    confidentiality_impact = models.CharField(max_length=15)
+    integrity_impact = models.CharField(max_length=15)
+    availability_impact = models.CharField(max_length=15)
+    source = models.CharField(max_length=50)
+    generated_datetime = models.DateTimeField()
+    cwe_id = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.cve_id
