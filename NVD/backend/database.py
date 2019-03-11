9e886e70c5a1f7e550e0575139fdf13f068b00f1
Konstantin <wetarmor@gmail.com>
2019-03-11 17:45:26 +0300

    Changed ORM from peewee to django built-in

--- NVD/backend/database.py
+++ NVD/backend/database.py
@@ -4,89 +4,21 @@ Database module.
 Used for making operations with PostgresQL.
 """
 
-import peewee
-from .models import CVE, db
+import os
 
-peewee.logger.disabled = True
+from . import models
 
-PG_CONFIG = {
-    "user": 'admin',
-    "password": '123',
-    "database": 'NVD',
-    "host": 'localhost',
-    "port": 5432,
-}
-
-database = peewee.PostgresqlDatabase(**PG_CONFIG)
-
-db.initialize(database)
-
-
-def connect_database(db) -> bool:
-    try:
-        if db.is_closed():
-            db.connect()
-        return True
-
-    except peewee.OperationalError as peewee_operational_error:
-        print("[-] Connect Postgres database error: %s", peewee_operational_error)
-
-    return False
-
-
-def disconnect_database(db) -> bool:
-    try:
-        if db.is_closed():
-            pass
-        else:
-            db.close()
-        return True
-
-    except peewee.OperationalError as peewee_operational_error:
-        print("[-] Disconnect Postgres database error: %s", peewee_operational_error)
-
-    return False
-
-
-def create_table():
-    connect_database(database)
-
-    if not CVE.table_exists():
-        CVE.create_table()
-
-    disconnect_database(database)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NVD.settings')
 
 
 def create_or_update_vulnerability(vulnerability: dict):
-    connect_database(database)
 
-    cve, created = CVE.get_or_create(
+    cve, created = models.CVE.objects.update_or_create(
         cve_id=vulnerability['cve_id'],
         defaults=vulnerability
     )
 
-    if not created:
-        (CVE.insert(**vulnerability).on_conflict(
-            conflict_target=(
-                CVE.cve_id
-            ),
-            update={
-                CVE.vendors: vulnerability['vendors'],
-                CVE.cwes: vulnerability['cwes'],
-                CVE.references: vulnerability['references'],
-                CVE.descriptions: vulnerability['descriptions'],
-                CVE.cpes: vulnerability['cpes'],
-                CVE.impact: vulnerability['impact'],
-                CVE.published_date: vulnerability['published_date'],
-                CVE.last_modified_date: vulnerability['last_modified_date']
-            }
-        ).execute())
-
-    disconnect_database(database)
-
 
 def create_or_update_vulnerabilities(vulnerabilities: list):
-    create_table()
-
     for vulnerability in vulnerabilities:
         create_or_update_vulnerability(vulnerability)
--- NVD/backend/models.py
+++ NVD/backend/models.py
@@ -1,35 +1,27 @@
-import peewee
-from playhouse.postgres_ext import ArrayField
-
-db = peewee.Proxy()
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
-class CVE(peewee.Model):
-    id = peewee.PrimaryKeyField(null=False)
-    cve_id = peewee.TextField(null=False, unique=True)
+class CVE(models.Model):
+    cve_id = models.TextField(null=False, unique=True)
     vendors = ArrayField(
-        peewee.TextField,
-        index_type='Hash'
+        models.TextField()
     )
     cwes = ArrayField(
-        peewee.TextField
+        models.TextField()
     )
     references = ArrayField(
-        peewee.TextField
+        models.TextField()
     )
     descriptions = ArrayField(
-        peewee.TextField
+        models.TextField()
     )
     cpes = ArrayField(
-        peewee.TextField
+        models.TextField()
     )
-    impact = peewee.TextField()
-    published_date = peewee.TextField(null=False)
-    last_modified_date = peewee.TextField(null=False)
+    impact = models.TextField()
+    published_date = models.TextField(null=False)
+    last_modified_date = models.TextField(null=False)
 
     def __str__(self):
         return self.cve_id
-
-    class Meta:
-        database = db
-        table_name = 'CVE'
