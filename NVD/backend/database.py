5c31058251dd7dac01cd7ae163219387c1959c22
Konstantin <wetarmor@gmail.com>
2019-03-31 20:10:19 +0300

    Added update_vulnerabilities_count function

--- NVD/backend/database.py
+++ NVD/backend/database.py
@@ -7,18 +7,32 @@ Used for making operations with PostgresQL.
 import os
 
 from . import models
+from .utils import get_year_from_filename
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NVD.settings')
 
 
-def create_or_update_vulnerability(vulnerability: dict):
+def update_vulnerabilities_count(filename: str, count: int):
+    year = get_year_from_filename(filename)
 
-    cve, created = models.CVE.objects.update_or_create(
+    if year:
+        models.YearsVulnerabilitiesCount.objects.update_or_create(
+            year=year[0],
+            vulnerabilities_count=count
+        )
+
+
+def create_or_update_vulnerability(vulnerability: dict):
+    models.CVE.objects.update_or_create(
         cve_id=vulnerability['cve_id'],
         defaults=vulnerability
     )
 
 
-def create_or_update_vulnerabilities(vulnerabilities: list):
+def create_or_update_vulnerabilities(vulnerabilities: list) -> int:
+    count = 0
     for vulnerability in vulnerabilities:
         create_or_update_vulnerability(vulnerability)
+        count += 1
+
+    return count
