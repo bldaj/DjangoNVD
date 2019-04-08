171a98178477e2f6f1bdb9e512a8ad1374adac66
Konstantin <wetarmor@gmail.com>
2019-04-08 11:14:59 +0300

    Included vendors_analyser task and temporary disabled other tasks

--- NVD/NVD/celery.py
+++ NVD/NVD/celery.py
@@ -12,14 +12,16 @@ app.autodiscover_tasks()
 app.config_from_object('django.conf:settings')
 
 app.conf.beat_schedule = {
-    'downloader': {
-        'task': 'backend.tasks.downloader',
-        'schedule': crontab(hour=1, minute=0),
-        'args': ()
-    },
-    'parser': {
-        'task': 'backend.tasks.parser',
-        'schedule': crontab(hour=1, minute=30),
-        'args': {}
+    # 'downloader': {
+    #     'task': 'backend.tasks.downloader',
+    #     'schedule': crontab(hour=18, minute=0),
+    # },
+    # 'parser': {
+    #     'task': 'backend.tasks.parser',
+    #     'schedule': crontab(hour=18, minute=5),
+    # },
+    'vendors_analyser': {
+        'task': 'backend.tasks.vendors_analyser',
+        'schedule': crontab(hour=22, minute=21)
     }
 }
