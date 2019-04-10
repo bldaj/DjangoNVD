b10f64829a63259f1ff8f0d7b0a2d9b3d4ac135b
Konstantin <wetarmor@gmail.com>
2019-04-10 16:16:13 +0300

    Returned tasks

--- NVD/NVD/celery.py
+++ NVD/NVD/celery.py
@@ -12,16 +12,16 @@ app.autodiscover_tasks()
 app.config_from_object('django.conf:settings')
 
 app.conf.beat_schedule = {
-    # 'downloader': {
-    #     'task': 'backend.tasks.downloader',
-    #     'schedule': crontab(hour=18, minute=0),
-    # },
-    # 'parser': {
-    #     'task': 'backend.tasks.parser',
-    #     'schedule': crontab(hour=18, minute=5),
-    # },
+    'downloader': {
+        'task': 'backend.tasks.downloader',
+        'schedule': crontab(hour=1, minute=0),
+    },
+    'parser': {
+        'task': 'backend.tasks.parser',
+        'schedule': crontab(hour=1, minute=30),
+    },
     'vendors_analyser': {
         'task': 'backend.tasks.vendors_analyser',
-        'schedule': crontab(hour=22, minute=21)
+        'schedule': crontab(hour=22, minute=0)
     }
 }
