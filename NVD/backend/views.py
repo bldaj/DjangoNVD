14a0b811946831c008f1a14629af7e47c53c50cd
Konstantin <wetarmor@gmail.com>
2019-06-05 17:40:22 +0300

    Added CVSSViewSet

--- NVD/backend/views.py
+++ NVD/backend/views.py
@@ -4,14 +4,16 @@ from rest_framework import viewsets, generics
 
 from . import models
 from . import serializers
+from .filters.cve import CVEFilter
 
 
 class CVEViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.CVE.objects.all()
     serializer_class = serializers.CVESerializer
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     search_fields = ('cve_id', 'vendors', 'cwes', 'descriptions')
     ordering_fields = ('cve_id',)
+    filter_class = CVEFilter
 
 
 class YearsVulnerabilitiesCountViewSet(generics.ListAPIView):
@@ -24,5 +26,12 @@ class YearsVulnerabilitiesCountViewSet(generics.ListAPIView):
 class MostVulnerableVendorsViewSet(generics.ListAPIView):
     queryset = models.MostVulnerableVendors.objects.all()
     serializer_class = serializers.MostVulnerableVendorsSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('year',)
+
+
+class CVSSViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.CVSS.objects.all()
+    serializer_class = serializers.CVSSSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('year',)
